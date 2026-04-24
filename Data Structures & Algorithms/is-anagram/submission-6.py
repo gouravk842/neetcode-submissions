7d4cd3eca9c002f8066c47f606a2e134867edc5c
gouravk842 <63838697+gouravk842@users.noreply.github.com>
2026-04-24 15:10:13 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26 # One slot for each letter 'a'-'z'
+        
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+            
+        return all(x == 0 for x in count)
+        
